d5676019b54e6a5ea77727c13e09fee343f3c282
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-15 03:12:05 +0100

    learning javascript

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
-// My first Code Challenge
+// conditions: If statements
 
-//Write code that will return a
-//random letter from my name Emmanuel
+//syntax
+/* if (condition) {
+    //run some code
+} else {
+    // run some different code
+} */
 
-const myName = "Emmanuel";
-const myNamelenght = myName.length;
-const randomNumber = Math.floor(Math.random() * myNamelenght);
-const charatMyname = myName.charAt(randomNumber);
+let customerIsBanned = false;
+let soup = "chicken noodle soup";
+let reply;
+let crackers = false;
 
-console.log(charatMyname);
+if (customerIsBanned) {
+  reply = `No soup for you!`;
+} else if (soup && crackers) {
+  reply = `Here's your order of ${soup} & crackers`;
+} else if (soup) {
+  reply = `Here's your order of ${soup} without crackers`;
+} else if (crackers) {
+  reply = `Here's your order of crackers`;
+} else {
+  reply = `Sorry, we are out of soup & crackers`;
+}
+
+console.log(reply);
