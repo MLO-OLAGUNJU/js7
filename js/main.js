78f679d7ca65d30ac5fdca35bd6cb326204ebaf3
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-15 03:19:50 +0100

    learning javascript

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
     // run some different code
 } */
 
-let customerIsBanned = false;
+/* let customerIsBanned = false;
 let soup = "chicken noodle soup";
 let reply;
 let crackers = false;
@@ -24,4 +24,25 @@ if (customerIsBanned) {
   reply = `Sorry, we are out of soup & crackers`;
 }
 
-console.log(reply);
+console.log(reply); */
+
+let testScore = 89;
+let grade;
+
+if (testScore >= 90) {
+  grade = `A1`;
+} else if (testScore < 90 && testScore >= 80) {
+  grade = `B2`;
+} else if (testScore < 80 && testScore >= 70) {
+  grade = `B3`;
+} else if (testScore < 70 && testScore >= 60) {
+  grade = `C4`;
+} else if (testScore < 60 && testScore >= 50) {
+  grade = `C5`;
+} else if (testScore < 50 && testScore >= 40) {
+  grade = `C6`;
+} else if (testScore < 70 && testScore >= 60) {
+  grade = `YOU ARE A FAILURE, KINDLY EXIT LIFE!!!!!!`;
+}
+
+console.log(grade);
